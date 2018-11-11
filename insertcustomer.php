803b544d6bcd0e661a7ad0a75ed45d0ec1de2895
Ivgeni Darinski <ivgeni.darinski@hotmail.com>
2018-11-11 16:54:48 -0500

    inserting a new customer

--- insertcustomer.php
+++ insertcustomer.php
@@ -11,9 +11,30 @@
     <title>EBAY - Add New Customer</title>
   </head>
   <body>
+    <!-- Connects database to document -->
     <?php
+      include 'connectdb.php';
+    ?>
+
+    <!-- PHP to insert new customer -->
+    <?php
+      # Variables initializing all new customer attributes
       $customerID = $_POST["newID"]
-      $query = 'INSERT INTO customer VALUES ()'
+      $customerFName = $_POST["firstName"];
+      $customerLName = $_POST["lastName"];
+      $customerAddress = $_POST["address"];
+      $customerPhone = $_POST["pNumber"];
+      $customerAgent = $_POST["agent"];
+
+      # Query to insert into customer table
+      $query = 'INSERT INTO customer VALUES (' . $customerID . ',' . $customerAgent . ',' $customerFName . ',' . $customerLName . ',' . $customerAddress . ',' . $customerPhone . ')';
+      $insert_result = mysqli_query($connection, $query);
+
+      # Checks if the query was successful
+      if ( !$insert_result ) {
+        die("Query to insert customer failed");
+      }
+
     ?>
 
   </body>
--- selectcustomer.php
+++ selectcustomer.php
@@ -9,17 +9,17 @@
 	# Runs a query to show all our customers ordered by the last name.
 	$query = 'SELECT * FROM customer INNER JOIN agent ON customersagentid=agentid ORDER BY lname';
 	$result = mysqli_query($connection, $query);
-	
+
 	# Checks if the query wasn't successful; prints error message
 	if (!$result) {
 		die("Customer Query Failed (selectcustomer.php)");
 	}
-	
+
 	#Loops through each row given in the query asked for above
 	while ($row = mysqli_fetch_assoc($result)) {
 		echo '<input type="radio" name="customers" value="' . $row["CustomerID"] . '" />' . $row["CustomerID"] . ':<br> Name: ' . $row["LName"] . ', ' . $row["FName"] . '<br>';
 		echo 'Agent: ' . $row["FirstName"] . ' ' . $row["LastName"] . '<br>';
 		echo 'Address: ' . $row["Address"] . '<br>';
-		echo 'Phone Number: (***) ' . $row["PhoneNumber"] . '<br><br>'; 
+		echo 'Phone Number: (***) ' . $row["PhoneNumber"] . '<br><br>';
 	}
 ?>
