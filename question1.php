6f6837d11966b9f3baaf52ee8a4517405b952614
Ivgeni Darinski <ivgeni.darinski@hotmail.com>
2018-11-13 16:21:38 -0500

    a

--- question1.php
+++ question1.php
@@ -34,7 +34,7 @@
     <?php
       include 'connectdb.php';
     ?>
-
+a
     <br>
     <!-- 1) Allows you to see the products a specific customer has purchased -->
     <form action="getproductsold.php" method="post">
