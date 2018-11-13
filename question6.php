0e99d892b75de80cffcaeaca8d867c160681ed8e
Ivgeni Darinski <ivgeni.darinski@hotmail.com>
2018-11-13 06:05:56 -0500

    hazzah!

--- question6.php
+++ question6.php
@@ -10,7 +10,7 @@
   <head>
     <meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="defaultstyle.css" />
-    <title>EBAY - Update Customer Phone</title>
+    <title>EBAY - Delete Customer</title>
   </head>
   <body>
     <!-- EBAY logo -->
