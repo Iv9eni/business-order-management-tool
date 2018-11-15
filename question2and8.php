58f0d3f7a62f13e95af9ce45dc9e9da3a52430ba
Ivgeni Darinski <ivgeni.darinski@hotmail.com>
2018-11-14 19:59:19 -0500

    kaka

---
+++ question2and8.php
@@ -0,0 +1,82 @@
+<!--
+  Author: Ivgeni Darinski
+  Student NO.: 250920885
+  Date Created: 2018-11-13
+  File: question2and8.php
+-->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="styling/defaultstyle.css" />
+		<link rel="stylesheet" type="text/css" href="styling/question2and8.css" />
+    <title>EBAY - Product Details</title>
+  </head>
+  <body>
+    <!-- EBAY logo -->
+    <img id="logo" src="images\logo.png">
+
+		<!-- Allows navigation through website -->
+		<div id="navigationBar">
+			<ul>
+        <li><a href="main\index.php">Home</a></li>
+				<li><a href="question1.php">View Customer Purchases</a></li>
+				<li><a class="active" href="question2and8.php">Products</a></li>
+				<li><a href="question3.php">Create Buy Order</a></li>
+				<li><a href="question4.php">Add Customer</a></li>
+				<li><a href="question5.php">Update Phone</a></li>
+        <li><a href="question6.php">Delete Customer</a></li>
+        <li><a href="question9.php">Order Summary</a></li>
+			</ul>
+		</div>
+
+    <!-- Connects to HTML file with the database we are working with -->
+    <?php
+      include 'connectdb.php';
+    ?>
+
+    <div id="wrapper">
+      <div id="orderByContainer">
+        <!-- Form to find products and order in a certain manner depicted by user -->
+        <form action="getproducts.php" method="post">
+          <p> ORDER PREFERENCES: </p>
+          <!-- Allows user to select from ascending/descending orders through a selection bar -->
+          <select name="order">
+            <option value="ASC">Ascending</option>
+            <option value="DESC">Descending</option>
+          </select>
+
+          <br>
+
+          <!-- Radio buttons to select which attribute to order the products by -->
+          <input type="radio" name="orderby" value="costperitem" checked="checked">ORDER BY PRICE<br>
+          <input type="radio" name="orderby" value="description">ORDER BY NAME<br>
+          <input id="submitStyle" type="submit" value = "Show Products">
+          <hr>
+        </form>
+      </div>
+
+
+      <div id="neverPurchasedContainer">
+        <p>ITEMS NEVER PURCHASED: </p><br>
+        <!-- Writes descriptions of products that were never purchased -->
+        <?php
+          # Query to find all products not in the list of purchased products
+          $query = 'SELECT * FROM product WHERE prodid NOT IN (SELECT productid FROM productsold)';
+          $result = mysqli_query($connection, $query);
+
+          # Starts an unordered list
+          echo '<ul>';
+          # Loops through table and prints every product that hasn't been purchased yet
+          while ($row = mysqli_fetch_assoc($result)) {
+            echo '<li>' . $row["Description"] . '</li>';
+          }
+          # Ends the unordered list
+          echo '</ul>';
+         ?>
+     </div>
+   </div>
+
+  </body>
+</html>
---
+++ question3.php
@@ -0,0 +1,85 @@
+<!--
+  Author: Ivgeni Darinski
+  Student NO.: 250920885
+  Date Created: 2018-11-13
+  File: question3.php
+-->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="styling/defaultstyle.css" />
+    <link rel="stylesheet" type="text/css" href="styling/question3.css" />
+    <title>EBAY - Create Buy Order</title>
+  </head>
+  <body>
+    <!-- EBAY logo -->
+    <img id="logo" src="images\logo.png">
+
+		<!-- Allows navigation through website -->
+		<div id="navigationBar">
+			<ul>
+        <li><a href="index.php">Home</a></li>
+				<li><a href="question1.php">View Customer Purchases</a></li>
+				<li><a href="question2and8.php">Products</a></li>
+				<li><a class="active" href="question3.php">Create Buy Order</a></li>
+				<li><a href="question4.php">Add Customer</a></li>
+				<li><a href="question5.php">Update Phone</a></li>
+        <li><a href="question6.php">Delete Customer</a></li>
+        <li><a href="question9.php">Order Summary</a></li>
+			</ul>
+		</div>
+
+    <!-- Connects to HTML file with the database we are working with -->
+    <?php
+      include 'connectdb.php';
+    ?>
+
+    <div id="wrapper">
+      <!-- Start of 3) -->
+  		<!-- To purchase a product for a customer -->
+  		<form action="insertpurchase.php" method="post">
+  			<?php
+  				# To display available customers to update
+  				include 'getcustomerdata.php';
+
+  				# Runs two querys to get all the customers and products that customers may purchase.
+  				$product_query = 'SELECT * FROM product';
+  				$p_result = mysqli_query($connection, $product_query);
+
+  				#Checks if product query was successful
+  				if ( !$p_result ) {
+  					die("Query 'SELECT * FROM product' FAILED");
+  				}
+
+  				# Starts a selection operation to pick from a list of customers
+  			  echo '<p style="display: block; margin-bottom: 20px;"> CUSTOMER </p><select name="customer" id="customerSelect">';
+  			  # Loops through list of customers and makes them options of our selection
+  			  while ($row = mysqli_fetch_assoc($c_result)) {
+  			    echo '<option value=' . $row["CustomerID"] . '>' . $row["FName"] . ' ' . $row["LName"] . '</option>';
+  			  }
+          # End of select statement
+  			  echo '</select>';
+
+          echo '<br>';
+
+
+          echo '<p style="margin-bottom: 30px; margin-top: 30px;"> PRODUCTS </p>';
+
+          # Putting all the radio buttons into an unordered list
+          echo '<ul>';
+  				# Loops through list of products and makes them options of our selection
+  				while ($row = mysqli_fetch_assoc($p_result)) {
+  					echo '<li><input type="radio" name="productsid" value=' . $row["ProdID"] . ' />' . $row["Description"] . ' $' . $row["CostPerItem"] . '</li>';
+  				}
+          echo '</ul>';
+  			?>
+
+  			<!-- Asks the user the quantity of the product chosen they would like to purchase -->
+  			<input type="text" name="quantity" placeholder="Quantity" id="quantityTxt" size="15">
+  			<br><input type="submit" value="Insert Product Purchase" id="subButton">
+  		</form>
+    </div>
+  </body>
+</html>
