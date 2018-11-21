983a71f825f31686c5461140026ee62d4284205c
Ivgeni Darinski <ivgeni.darinski@hotmail.com>
2018-11-21 11:09:29 -0500

    da kush

--- question2and8.php
+++ question2and8.php
@@ -105,10 +105,9 @@
 
          # Runs a query to find all the products and customers
          $search_query = 'SELECT fname, lname, description, quantity FROM productsold INNER JOIN product ON productid=prodid INNER JOIN customer ON purchaserid=customerid WHERE quantity>=' . $quantity;
-         $result = mysqli_query($connection, $search_query);
 
          # Checks if the query was successful
-         if ($result) {
+         if ($result = mysqli_query($connection, $search_query)) {
 
            # Starts an unordered list
            echo '<ul>';
